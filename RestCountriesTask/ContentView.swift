e1ff00709cc389cbde47d5488c5b5bcf7f169e24
Sherif Hamdy <sherif.hamdy.ahmed.93@gmail.com>
2025-12-01 19:55:19 +0200

    Initial Commit

---
+++ RestCountriesTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RestCountriesTask
+//
+//  Created by Sherif Hamdy on 01/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RestCountriesTask/RestCountriesTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestCountriesTaskApp.swift
+//  RestCountriesTask
+//
+//  Created by Sherif Hamdy on 01/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct RestCountriesTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
