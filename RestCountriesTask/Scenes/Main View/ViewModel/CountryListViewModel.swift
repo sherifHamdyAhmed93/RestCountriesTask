114ae91057e6dc1912e82b09e61f2f64e1676423
Sherif Hamdy <sherif.hamdy.ahmed.93@gmail.com>
2025-12-02 02:49:25 +0200

    Add view model to get all countries , display main countries and search for countries

---
+++ RestCountriesTask/Scenes/Main View/ViewModel/CountryListViewModel.swift
@@ -0,0 +1,92 @@
+//
+//  CountryListViewModel.swift
+//  RestCountriesTask
+//
+//  Created by Sherif Hamdy on 02/12/2025.
+//
+
+import Foundation
+import Combine
+
+final class CountryListViewModel: ObservableObject {
+    private(set) var countries: [CountryUIModel] = []
+    @Published private(set) var mainCountries:[CountryUIModel] = []
+    @Published private(set) var filteredCountries:[CountryUIModel] = []
+
+    @Published private(set) var isLoading:Bool = false
+    @Published var query:String = ""
+    @Published private(set) var error:String = ""
+    
+    private let countryService:CountryServiceProtocol
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(countryService: CountryServiceProtocol = CountryService()) {
+        self.countryService = countryService
+        bindSearchQuery()
+    }
+    
+    private func bindSearchQuery(){
+        $query
+            .debounce(for: .seconds(1), scheduler: RunLoop.main)
+            .removeDuplicates()
+            .filter {
+                $0.isEmpty == false
+            }
+            .sink { [weak self] country in
+                self?.search(for: country)
+            }
+            .store(in: &cancellables)
+    }
+    
+    func loadCountires() {
+        isLoading = true
+        countryService.getAllCountries()
+            .sink { [weak self] completion in
+                self?.isLoading = false
+                switch completion{
+                case .finished:
+                    print("Finished")
+                case .failure(let error):
+                    self?.isLoading = false
+                    self?.error = error.localizedDescription
+                    print("Error : \(error.localizedDescription)")
+                }
+            } receiveValue: { [weak self]response in
+                self?.countries = response.map({
+                    CountryUIModel(from: $0)
+                })
+            }
+            .store(in: &cancellables)
+    }
+    
+    func resetFilteredCountries(){
+        self.filteredCountries = []
+    }
+    
+    private func search(for name:String){
+        self.resetFilteredCountries()
+        guard !name.isEmpty else{
+            self.filteredCountries = self.countries
+            return
+        }
+        let result =  countries.filter { country in
+            return country.countryName.lowercased().contains(name.lowercased())
+        }
+        self.filteredCountries = result
+    }
+    
+    func addCountry(_ country: CountryUIModel) {
+        guard mainCountries.count < 5 else { return }
+        if !mainCountries.contains(country) {
+            mainCountries.append(country)
+        }
+    }
+    
+    func deleteCity(at offsets: IndexSet) {
+        offsets.forEach { index in
+            mainCountries.remove(at: index)
+        }
+    }
+    
+}
